c9fc88980d4c107af436670f5629db1e05e4932a
Passwordis1 <passwordis1@Passwordis1s-MacBook-Pro.local>
2018-02-18 11:35:27 +0200

    index fix

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -6,7 +6,8 @@ import {
   FirebaseListObservable
 } from 'angularfire2/database-deprecated';
 import { ActivatedRoute, ActivatedRouteSnapshot } from '@angular/router';
-
+import { SwPush, SwUpdate } from '@angular/service-worker';
+import { Subscription } from 'rxjs/Subscription';
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
@@ -16,13 +17,21 @@ export class ChatComponent {
   messages: FirebaseListObservable<any[]>;
   name: string;
   message: FormControl = new FormControl();
-  constructor(db: AngularFireDatabase, ar: ActivatedRoute) {
+  constructor(db: AngularFireDatabase, ar: ActivatedRoute, sw: SwPush) {
     this.name = ar.snapshot.params['name'];
+    sw.requestSubscription({ serverPublicKey: '123456' }).then(value => {
+      console.log('sdsds', value);
+    });
+    sw.subscription.subscribe(subscription => {
+      console.log('subs', subscription);
+    });
     this.messages = db.list('messages');
+    sw.messages.subscribe(message => {
+      console.log(message);
+    });
   }
 
   send(text) {
-    console.log(event);
     this.messages.push({
       text,
       name: this.name,
